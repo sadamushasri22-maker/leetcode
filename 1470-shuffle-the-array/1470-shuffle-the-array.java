4ed91291cc15a0a350c2207e1d7403cb01f224a8
sadamushasri22-maker <sadamushasri22@gmail.com>
2026-06-09 11:16:57 +0530

    Time: 0 ms (100%), Space: 46.4 MB (60.41%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -1,11 +1,9 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] a = new int[2 * n];
-        
-        // Changed i = i + 2 to i++ to visit every index sequentially
-        for (int i = 0; i < n; i++) {
-            a[2 * i] = nums[i];       // Places x1, x2, x3...
-            a[2 * i + 1] = nums[n + i]; // Places y1, y2, y3...
+          for (int i = 0; i < n; i++) {
+            a[2 * i] = nums[i];   
+            a[2 * i + 1] = nums[n + i];  
         }
         
         return a;
